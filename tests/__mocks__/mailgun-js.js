fbabaec74f0a2f06dad12e94da9e7745bcf4d9c7
teredet <artem.vanzytler@gmail.com>
2023-08-24 11:38:46 +0300

    Mocking mailgun lib.

---
+++ tests/__mocks__/mailgun-js.js
@@ -0,0 +1,16 @@
+export default function (apiKey, domain) {
+    console.log('mailgun-js.js')
+    const object2 = {
+        send() {
+            
+        }
+    }
+
+    const object1 = {
+        messages() {
+            return object2
+        }
+    }
+
+    return object1
+}
